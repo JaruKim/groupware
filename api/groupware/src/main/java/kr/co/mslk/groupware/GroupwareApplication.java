ba66b911d10dcea32be96784c91f6da2d22887cc
khkim <khkim@mslk.co.kr>
2021-09-06 00:15:40 +0900

    api 프로젝트 세팅

---
+++ api/groupware/src/main/java/kr/co/mslk/groupware/GroupwareApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mslk.groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupwareApplication.class, args);
+	}
+
+}
